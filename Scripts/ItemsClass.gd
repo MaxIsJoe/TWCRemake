b76e50158a4615ea2e8b82f98c525800d0b1ce8b
MaxIsJoe <maxisjoe130@gmail.com>
2018-10-20 18:58:35 +0200

    Added an Item class
    Found something that's like scriptable objects from HeartBeast and thought it would work well with this (I think)

---
+++ Scripts/ItemsClass.gd
@@ -0,0 +1,9 @@
+extends Resource
+class_name Items
+
+export (bool) var IsImportant
+export (bool) var CanBeThrown
+export (int) var value
+export (String) var name
+export (String) var desc
+export (Image) var icon
