9f157028a169f83449c4b7cbffe5cedbfe02f89e
MaxIsJoe <maxisjoe130@gmail.com>
2019-01-03 22:30:22 +0200

    Added doors

---
+++ Scenes/Instances/Interactive/Doors/Door.gd
@@ -0,0 +1,35 @@
+extends Node2D
+
+#onready var Colider = $Collision
+onready var AreaCollision = $Area2D/AreaCollision
+onready var spriteanim = $AnimatedSprite
+onready var timer = $Timer
+
+#export(bool) var CanBeBroken = false
+
+var IsOpen
+#var playerlist
+#var DoorHealth = 3000
+
+
+func _ready():
+	IsOpen = false
+	spriteanim.play("Idle")
+	
+
+
+func _on_Area2D_body_entered(body):
+	if(body.is_in_group("Players")):
+		IsOpen = true
+		#Colider.set_disabled(true)
+		AreaCollision.set_disabled(true)
+		spriteanim.play("Opening")
+		timer.start()
+		
+
+func _on_Timer_timeout():
+	spriteanim.play("Closing")
+	IsOpen = false
+	AreaCollision.set_disabled(false)
+	
+	
