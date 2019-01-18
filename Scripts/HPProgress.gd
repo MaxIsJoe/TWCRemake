e133b1ac1da8d3b663daa8b2e5d8e70115d43266
MaxIsJoe <maxisjoe130@gmail.com>
2019-01-18 23:56:58 +0200

    Added animations to the player Health and mana bars
    it's a simple tween but it works.

--- Scripts/HPProgress.gd
+++ Scripts/HPProgress.gd
@@ -1,7 +1,11 @@
 extends TextureProgress
 
+onready var tween = $Tween
 
 func _on_Player_hpupdate(health):
-	value = health
-
+	#value = health
+	tween.interpolate_property(self, "value",
+        value, health, 1,
+        Tween.EASE_OUT , Tween.EASE_IN_OUT)
+	tween.start()
 
--- Scripts/MPProgress.gd
+++ Scripts/MPProgress.gd
@@ -1,5 +1,10 @@
 extends TextureProgress
 
+onready var tween = $Tween
 
 func _on_Player_mpupdate(mana):
-	value = mana
+	#value = mana
+	tween.interpolate_property(self, "value",
+        value, mana, 1,
+        Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
