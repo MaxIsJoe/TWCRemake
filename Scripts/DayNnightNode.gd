2a61177f510e28f6e2468bf3668ae2c72c609965
MaxIsJoe <maxisjoe130@gmail.com>
2019-01-19 19:06:42 +0200

    Continued working on the Day N Night system

--- Scripts/DayNnightNode.gd
+++ Scripts/DayNnightNode.gd
@@ -1,10 +1,32 @@
-extends Light2D
+extends Sprite
 
 onready var DaynNightbehavior = get_node("/root/dayNnight")
 
+var IsDay = true
+var IsNight = false
+
 func _ready():
-	pass
-	#if(DaynNightbehavior.Day):
-		#Light2D.Color = transparentcolor
-	#if(DayNightbehavior.Night):
-		#apply the same color as the night's color in the orignal game
+	if(DaynNightbehavior.Day):
+		DaynNightbehavior.ChangeToDay(self)
+	else:
+		DaynNightbehavior.ChangeToNight(self)
+	print(modulate)
+	
+func _process(delta):
+	DaynNightbehavior.TimeMove()
+	if(DaynNightbehavior.Day):
+		if(IsDay):
+			print("Has checked day")
+		if(!IsDay):
+			print("changed to Night")
+			DaynNightbehavior.ChangeToNight(self)
+			IsDay = false
+			IsNight = true
+	if(DaynNightbehavior.Night):
+		if(IsNight):
+			print("has changed night")
+		if(!IsNight):
+			print("has changed to day")
+			DaynNightbehavior.ChangeToDay(self)
+			IsDay = true
+			IsNight = false
--- Scripts/dayNnight.gd
+++ Scripts/dayNnight.gd
@@ -3,24 +3,33 @@ extends Node
 var Day = true
 var Night = false
 var timer
-var WaitForNextTimeOfDay = 3000
+var WaitForNextTimeOfDay = 4
 
 func _ready():
 	timer = Timer.new()
 	timer.connect("timeout",self,"_on_timer_timeout") 
 	add_child(timer)
 	timer.wait_time = WaitForNextTimeOfDay
+	timer.one_shot = false
 	timer.start()
 
 func _on_timer_timeout():
    TimeMove()
+   print("Ding!")
 
 func TimeMove():
-	if(Day):
+	if(Day == false):
 		Night = true
 		Day = false
-		return
-	if(Night):
+		print(Day, Night)
+	if(Night == false):
 		Day = true
 		Night = false
-		return
+		print(Day, Night)
+
+
+func ChangeToDay(sprite):
+	sprite.modulate = Color( 225, 225, 225, 0 )
+	
+func ChangeToNight(sprite):
+	sprite.modulate = Color( 17, 5, 73, 121)
