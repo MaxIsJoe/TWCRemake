739eb93e68f7bb75a762da355ab8397f044c74b8
MaxIsJoe <maxisjoe130@gmail.com>
2019-04-02 03:27:35 +0200

    Added the house points system
    should be fully functional now

---
+++ Scripts/Global.gd
@@ -0,0 +1,51 @@
+extends Node
+
+#House Points
+var GrifPoints = 0
+var SlythPoints = 0
+var ClawPoints = 0
+var HuffPoints = 0
+
+var housewinnerofthismonth
+
+var housenames = ["Grif","Slyth","Claw","Huff"]
+
+var day = OS.get_date()["day"]
+
+
+func givepoints(points, to_house):
+	if(to_house == "Grif"):
+		GrifPoints += points
+	elif(to_house == "Slyth"):
+		SlythPoints += points
+	elif(to_house == "Huff"):
+		HuffPoints += points
+	elif(to_house == "Claw"):
+		ClawPoints += points
+
+func takepoints(points, from_house):
+	if(from_house == "Grif"):
+		GrifPoints -= points
+	elif(from_house == "Slyth"):
+		SlythPoints -= points
+	elif(from_house == "Huff"):
+		HuffPoints -= points
+	elif(from_house == "Claw"):
+		ClawPoints -= points
+		
+func GiveHouseCup():
+	var points = [GrifPoints, SlythPoints, ClawPoints, HuffPoints] #stores all variables in a array to be quick
+	var winner = max(0, points) #the winner has more points than zero and is greater that all other points
+	for i in range(0,points.size()): #checks for a winner with the greatest points
+		if (winner == points[i]):
+			housewinnerofthismonth = housenames[i] #the winner is the same index in both points and houses array
+			break #stops the loop once it finds a winner
+
+
+
+#Updates
+
+func _process(delta):
+	day = OS.get_date()["day"]
+	if(day == 29): #on the 29th of each month the house cup will be given to the house with most points
+		GiveHouseCup()
