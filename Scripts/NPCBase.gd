2c3a48c64b9aef67891710d6edd20323733846cc
MaxIsJoe <maxisjoe130@gmail.com>
2019-04-03 02:59:24 +0200

    Finished the basic quick dialouge system.

--- Scripts/NPCBase.gd
+++ Scripts/NPCBase.gd
@@ -3,6 +3,7 @@ extends KinematicBody2D
 onready var Quick_Dia = $Quick_Dia
 onready var Dialoug = $Holders/DialougHolder
 onready var TimerNode = $Timer
+onready var VisableCharactersTimer = $Quick_Dia/Visable_Chars
 
 export(String) var NPC_Name = "NPC"
 export(String) var Desc = "No description"
@@ -20,4 +21,9 @@ func _on_Timer_timeout():
 
 
 func _on_DialougHolder_send_text(text):
+	Quick_Dia.visible_characters = 3000
 	Quick_Dia.bbcode_text = "[center]" + text
+	VisableCharactersTimer.start()
+
+func _on_Visable_Chars_timeout():
+	Quick_Dia.visible_characters = 0
