3f8e972a373fdc281ef370c7c593b8928aba31f9
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-21 07:00:10 +0200

    Added in a general JSON Loader

---
+++ Scripts/Globals/Data.gd
@@ -0,0 +1,10 @@
+extends Node
+
+###Data###
+##Here all data that needs to be stored will be put here.##
+
+#Dialouge#
+var Loaded_Dialouge
+
+#Inventory and Items#
+var ItemData
---
+++ Scripts/JSON-Systems/JSON_LOADER.gd
@@ -0,0 +1,20 @@
+extends Node
+
+
+func LoadJSON_General(JSONFile, WhereToStore): #Used to pass data to variables that can be accsed globally
+	print("[LOAD] file : looking for [%s]..." % JSONFile)
+	var file = File.new()
+	var dfile = file.open(JSONFile, File.READ)
+	if(dfile == OK):
+		print("[SUCCSUES] file : [%s] does exist and is loaded." % JSONFile)
+		dfile = file.get_as_text()
+		var data = parse_json(dfile)
+		match WhereToStore:
+			0: #Dialouge
+				Data.Loaded_Dialouge = data
+			1: #Items
+				Data.ItemData = data
+		file.close()
+	else:
+		file.close()
+		print("[ERROR] file : [%s] does not exist or is empty" % JSONFile)
