317e2fcbf5320d51d7aa58d5b486c591f3195dc3
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-21 06:23:40 +0200

    Removed old DnN check

--- Scripts/StepCheck.gd
+++ Scripts/StepCheck.gd
@@ -16,11 +16,3 @@ func _on_StepCheck_body_shape_exited(body_id, body, body_shape, area_shape):
 		BodiesOnTop.erase(body)
 		body.modulate = Color(1, 1, 1, 1)
 		print("changed color")
-		
-func _process(delta):
-	if(dayNnight.Day):
-		for p in BodiesOnTop:
-			p.modulate = Color(1, 1, 1, 1)
-	if(dayNnight.Night):
-		for p in BodiesOnTop:
-			p.modulate = Color(0.14, 0.52, 0.88, 0.90)
