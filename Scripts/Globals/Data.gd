576451e605aa499eaad9743b86f15923bfce73bf
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-22 05:46:35 +0200

    Added a reference to the Items.JSON file

--- Scripts/Globals/Data.gd
+++ Scripts/Globals/Data.gd
@@ -1,10 +1,13 @@
 extends Node
 
-###Data###
-##Here all data that needs to be stored will be put here.##
+####Data####
+##Here all data that needs to be stored and referenced will be put here.##
 
+#Always present#
+#Inventory and Items#
+var ItemJSON = "res://Resources/Items/Items.JSON"
+var ItemData
+
+#Temp#
 #Dialouge#
 var Loaded_Dialouge
-
-#Inventory and Items#
-var ItemData
