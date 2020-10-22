2c8771820a3ae2357f5dd23635ad0471cad1ba75
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-22 06:49:12 +0200

    Clicking on an item now will display it's description and name

--- Scenes/Instances/Actors/player/ItemButton.gd
+++ Scenes/Instances/Actors/player/ItemButton.gd
@@ -15,4 +15,12 @@ func UpdateButton(ItemID):
 
 
 func _on_ItemButton_button_down():
-	pass # Replace with function body.
+	var p = get_tree().get_nodes_in_group("UI_ItemDescPanel")
+	var v = get_tree().get_nodes_in_group("UI_ItemIconPanel")
+	var a = get_tree().get_nodes_in_group("UI_ItemNamePanel")
+	for d in p:
+		d.bbcode_text = Data.ItemData[ID].get("desc")
+	for d in v: #Noticed the joke? Yes.. I know, I'm unfunny.
+		d.texture = load(Data.ItemData[ID].get("icon"))
+	for d in a:
+		d.text = Data.ItemData[ID].get("name")
