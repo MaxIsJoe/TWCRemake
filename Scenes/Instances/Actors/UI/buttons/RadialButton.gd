0b3c5453130b6d9ec36c431829502a7168575547
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-27 10:56:02 +0200

    Added grab functionality for Items in the radial menus
    Works like a charm

--- Scenes/Instances/Actors/UI/buttons/RadialButton.gd
+++ Scenes/Instances/Actors/UI/buttons/RadialButton.gd
@@ -1,4 +1,13 @@
 extends Button
 
+var ID
+
 func UpdateButtionUI(img):
 	$TextureRect.texture = load(img)
+
+
+func _on_RadialButton_button_down():
+	match $TextureRect.texture.resource_path:
+		Data.UI_Icon_Grab:
+			Data.Player.grab(ID)
+			get_parent().get_parent().get_parent().queue_free()
