a947221f165e8681dc10a9be95a0e9486dc0f68e
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-27 10:54:17 +0200

    Removed magnet pickup functionality
    Radial menus are used to grab items now

--- Scripts/ItemsClass.gd
+++ Scripts/ItemsClass.gd
@@ -9,12 +9,6 @@ func _ready():
 	$RadialMenu.LoadButton("Grab", ItemID)
 
 
-func _on_Item_body_entered(body):
-	if(body.is_in_group("Players")):
-		body.grab(ItemID)
-		queue_free()
-		##This will be changed later, the player should grab the item when they're close to it using a hotkey or the classic drop down menu, but for now we'll work this this.##
-
 
 func _on_Item_input_event(viewport, event, shape_idx):
 	var distance2player = Global.GetDistance2Player(self)
