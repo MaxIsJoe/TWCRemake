1ce35c7d0542852af30eecc5663d2bd18ce66a9d
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 09:43:49 +0200

    Day and Night system finally added

---
+++ Scenes/Instances/World/DayAndNight.gd
@@ -0,0 +1,27 @@
+extends ColorRect
+
+export(float) var ShortDuration = 1
+export(float) var LongDuration = 3
+export(Color) var DayColor
+export(Color) var NightColor
+export(Color) var DawnColor
+export(Color) var MidnightColor
+export(Color) var BloodMoon
+
+func ChangeColor(color):
+	match color:
+		"Day":
+			$Tween.interpolate_property(self, "color", self.color, DayColor, ShortDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Night":
+			$Tween.interpolate_property(self, "color", self.color, NightColor, LongDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Dawn":
+			$Tween.interpolate_property(self, "color", self.color, DawnColor, LongDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Midnight":
+			$Tween.interpolate_property(self, "color", self.color, MidnightColor, ShortDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Bloodmoon":
+			$Tween.interpolate_property(self, "color", self.color, MidnightColor, ShortDuration,Tween.TRANS_BOUNCE,Tween.EASE_IN_OUT)
+			$Tween.start()
---
+++ Scripts/Globals/DayAndNight.gd
@@ -0,0 +1,68 @@
+extends Node
+
+enum TimeOfDay  {
+	Day,
+	Dusk,
+	Dawn,
+	Night,
+	Midnight	
+}
+
+var TimeLong = 10
+var TimeShort = 5
+
+var currentTime = TimeOfDay.Day
+
+var timer
+
+
+func _ready():
+	timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(timer)
+	timer.wait_time = TimeShort
+	timer.one_shot = false
+	timer.start()
+	print("[WORLD] : Day And Night is ready!")
+	print("[WORLD] : Timer wait_time = " + str(timer.wait_time))
+	
+func UpdateAllNodes(Time):
+	var DNN_Nodes = get_tree().get_nodes_in_group("DNN")
+	for node in DNN_Nodes:
+		node.ChangeColor(Time)
+
+func ChangeTime(Time):
+	match Time:
+		TimeOfDay.Day:
+			UpdateAllNodes("Day")
+			currentTime = TimeOfDay.Day
+		TimeOfDay.Dusk:
+			UpdateAllNodes("Dawn")
+			currentTime = TimeOfDay.Dusk
+		TimeOfDay.Night:
+			UpdateAllNodes("Night")
+			currentTime = TimeOfDay.Night
+		TimeOfDay.Midnight:
+			UpdateAllNodes("Midnight")
+			currentTime = TimeOfDay.Midnight
+		TimeOfDay.Dawn:
+			UpdateAllNodes("Dawn")
+			currentTime = TimeOfDay.Dawn
+
+func _on_timer_timeout():
+	match currentTime:
+		TimeOfDay.Day:
+			ChangeTime(TimeOfDay.Dusk)
+			timer.wait_time = TimeShort
+		TimeOfDay.Night:
+			ChangeTime(TimeOfDay.Midnight)
+			timer.wait_time = TimeLong
+		TimeOfDay.Midnight:
+			ChangeTime(TimeOfDay.Dawn)
+			timer.wait_time = TimeShort
+		TimeOfDay.Dusk:
+			ChangeTime(TimeOfDay.Night)
+			timer.wait_time = TimeLong
+		TimeOfDay.Dawn:
+			ChangeTime(TimeOfDay.Day)
+			timer.wait_time = TimeLong
