3e64a91c3859790063fa5c9f5dfaf32c63ccb851
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 17:52:47 +0200

    Added some random pitch for footsteps

--- Scenes/Instances/World/Teleport/TeleportLocalNode.gd
+++ Scenes/Instances/World/Teleport/TeleportLocalNode.gd
@@ -2,12 +2,12 @@ extends Area2D
 
 export(NodePath) var TeleportLocation
 export(AudioStream) var Footstep_Sounds
-
-func _ready():
-	$AudioStreamPlayer2D.stream = Footstep_Sounds
+#export(Range) var Pitch_Range
 
 func _on_TeleportLocalNode_body_entered(body):
 	if(body.is_in_group("Players")):
 		if(Footstep_Sounds != null):
-			$AudioStreamPlayer2D.play()
+			randomize()
+			var pitch = rand_range(-2,2)
+			GlobalAudio.PlaySound(Footstep_Sounds,$AudioStreamPlayer2D, null, null, pitch)
 		body.position = get_node(TeleportLocation).position
