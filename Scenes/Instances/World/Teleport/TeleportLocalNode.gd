8cc3ca2f7b9d8cf1575c63b33be844adb4c02c61
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 17:20:16 +0200

    An optional audio que can now be played locally when using TeleNodes

--- Scenes/Instances/World/Teleport/TeleportLocalNode.gd
+++ Scenes/Instances/World/Teleport/TeleportLocalNode.gd
@@ -1,8 +1,13 @@
 extends Area2D
 
 export(NodePath) var TeleportLocation
+export(AudioStream) var Footstep_Sounds
 
+func _ready():
+	$AudioStreamPlayer2D.stream = Footstep_Sounds
 
 func _on_TeleportLocalNode_body_entered(body):
 	if(body.is_in_group("Players")):
+		if(Footstep_Sounds != null):
+			$AudioStreamPlayer2D.play()
 		body.position = get_node(TeleportLocation).position
