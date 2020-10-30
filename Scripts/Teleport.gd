d6a0f73ce994040b22d351329bceefec3c2ba3a8
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 19:28:01 +0200

    Move_To_Scene is now functional

--- Scripts/Teleport.gd
+++ Scripts/Teleport.gd
@@ -5,14 +5,17 @@ var spawnpos
 
 
 func Move_To_Scene(nextscene, player,spawnPos):
-	#call_deferred("Deffered_MoveToScene", nextscene, player,spawnPos)
-	Deffered_MoveToScene(nextscene, player,spawnPos)
+	call_deferred("Deffered_MoveToScene", nextscene, player,spawnPos)
+	#Deffered_MoveToScene(nextscene, player,spawnPos)
 	
 func Deffered_MoveToScene(nextscene, player,spawnPos):
 	var scene = nextscene
 	pl = player
 	print("Chaning Scene..")
+	player.get_parent().remove_child(player)
+	get_tree().get_root().add_child(pl)
 	get_tree().change_scene(nextscene) #<--- Stops here
-	get_tree().get_root().add_child(pl) #<-- Player doesn't get added
+	print("change_scene test print")
+	get_tree().get_root().add_child(player) #<-- Player doesn't get added
 	print("we reached this point") #<--- Doesn't get printed
 	player.position = spawnPos
