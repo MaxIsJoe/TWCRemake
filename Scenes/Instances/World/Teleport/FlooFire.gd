324aaa50164fae1a58e21385b3a2f950e8383163
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 19:30:37 +0200

    Teleport Postion is now editable in the inspector

--- Scenes/Instances/World/Teleport/FlooFire.gd
+++ Scenes/Instances/World/Teleport/FlooFire.gd
@@ -1,11 +1,11 @@
 extends Node2D
 
 export(String) var TeleportToScene
-
+export(Vector2) var TeleportLocation
 
 
 
 
 func _on_Area2D_body_entered(body):
 	if(body.is_in_group("Players")):
-		Teleport.Move_To_Scene(TeleportToScene, body, Vector2(0,0))
+		Teleport.Move_To_Scene(TeleportToScene, body, TeleportLocation)
