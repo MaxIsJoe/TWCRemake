3e6eaf7b07c2243935c26a631ae42f1ce92b002b
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-30 09:45:47 +0200

    Inventory Buttons now have IDs

--- Scripts/Inventory.gd
+++ Scripts/Inventory.gd
@@ -7,10 +7,13 @@ onready var panel = $Panel/VBoxContainer
 
 export(PackedScene) var ItemButton
 
+var ButtonIDs = -1
+
 func create_item_button(item):
+	ButtonIDs += 1
 	var item_button = ItemButton.instance()
 	panel.add_child(item_button)
-	item_button.UpdateButton(item)
+	item_button.UpdateButton(item, ButtonIDs)
 	return item_button
 
 func _on_Player_GrabbedAnItem(item):
