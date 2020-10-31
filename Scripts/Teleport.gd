7bc0a58286ad1acf245c14a36f5120a2fde23439
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-31 10:54:35 +0200

    Fixed a rare crash when moving between scenes

--- Scripts/Teleport.gd
+++ Scripts/Teleport.gd
@@ -10,12 +10,13 @@ func Move_To_Scene(nextscene, player,spawnPos):
 	
 func Deffered_MoveToScene(nextscene, player,spawnPos):
 	var scene = nextscene
-	pl = player
+	pl = Data.Player
 	print("Chaning Scene..")
 	player.get_parent().remove_child(player)
 	get_tree().get_root().add_child(pl)
 	get_tree().change_scene(nextscene) #<--- Stops here
 	print("change_scene test print")
-	get_tree().get_root().add_child(player) #<-- Player doesn't get added
+	get_tree().get_root().add_child(pl) #<-- Player doesn't get added
 	print("we reached this point") #<--- Doesn't get printed
-	player.position = spawnPos
+	pl.position = spawnPos
+	Data.Player = pl
