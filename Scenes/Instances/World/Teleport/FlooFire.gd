d3c8a5740aa0bfa50711c1ee5b47e81725d5b7ea
MaxIsJoe <maxisjoe130@gmail.com>
2020-10-31 10:55:25 +0200

    Added a sound for moving between scenes

--- Scenes/Instances/World/Teleport/FlooFire.gd
+++ Scenes/Instances/World/Teleport/FlooFire.gd
@@ -8,4 +8,5 @@ export(Vector2) var TeleportLocation
 
 func _on_Area2D_body_entered(body):
 	if(body.is_in_group("Players")):
+		GlobalAudio.PlaySound(load("res://Sound/SFX/teleports/poof_simple.wav"), Data.Player.AudioLocal, null, null, null)
 		Teleport.Move_To_Scene(TeleportToScene, body, TeleportLocation)
