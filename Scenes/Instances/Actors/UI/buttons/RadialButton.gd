7f07475e1ceb25addc2245eed42bf0184c40e279
MaxIsJoe <maxisjoe130@gmail.com>
2020-11-02 09:27:53 +0200

    Radial menus will now close after they have been used

--- Scenes/Instances/Actors/UI/buttons/RadialButton.gd
+++ Scenes/Instances/Actors/UI/buttons/RadialButton.gd
@@ -15,3 +15,4 @@ func _on_RadialButton_button_down():
 			Global.LoadDialouge(get_parent().get_parent().get_parent().Dialoug.DialougFile, "000")
 		Data.UI_Icon_Shop:
 			Data.Player.ShowShopUI(get_parent().get_parent().get_parent().shop.ItemIDs, get_parent().get_parent().get_parent().shop.ShopID)
+	get_parent().get_parent().ToggleButtonVisbility(false)
