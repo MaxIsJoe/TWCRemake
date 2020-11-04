4537e89a2a13840748b9690ea817c843f67efcb9
MaxIsJoe <maxisjoe130@gmail.com>
2020-11-04 14:27:13 +0200

    updating player scenes

--- Scenes/Instances/Actors/NPCs/ShopHolder.gd
+++ Scenes/Instances/Actors/NPCs/ShopHolder.gd
@@ -1,7 +1,10 @@
 extends Node
 
+##This holds all the shop can sell and what is their ID
+
 export(int) var ShopID = 0
 export(Array, String) var ItemIDs
 
 func _ready():
 	Data.Update_Group_ShopHolders()
+
