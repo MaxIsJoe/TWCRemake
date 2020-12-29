6009b02c3d4cee597396d69a5825627baeec84e3
MaxIsJoe <maxisjoe130@gmail.com>
2020-12-29 23:32:26 +0200

    Removed the host button

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -39,15 +39,3 @@ func LoadMap(n):
 	call_deferred("add_child", menu)
 	loadingbar.value += 25
 	FirstLoadUI.queue_free()
-
-func _on_Host_button_down():
-	#if $FirstLoad/VBoxContainer/HBoxContainer2/ID_Text.text == "":
-	#	return
-	#Network.create_server($FirstLoad/VBoxContainer/HBoxContainer2/ID_Text.text)
-	Network.create_server()
-	var note = RichTextLabel.new()
-	note.bbcode_enabled = true
-	note.bbcode_text = "[color=green]You are now hosting locally, online hosting is not implamented yet.[/color]"
-	$FirstLoad/VBoxContainer/HBoxContainer/Host.disabled = true
-	FirstLoadUI.add_child(note)
-	note.rect_size = Vector2(1000,400)
