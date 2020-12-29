72ee9f84ebd17e579daa5d3c65ce79dd019d9985
MaxIsJoe <maxisjoe130@gmail.com>
2020-12-29 21:25:31 +0200

    Fixed some consistancy issues reported in #15

--- Scripts/Globals/Networking/NetworkingFunctions.gd
+++ Scripts/Globals/Networking/NetworkingFunctions.gd
@@ -5,8 +5,8 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 	if(Network.PlayerContainer.has_node(str(network_id)) or charname == null): #To (hopefully) prevent the game from creating millions of copies of the same player.
 		return
 	else:
-		if(gender == 1):
-			if(house == 1):
+		if(gender == 0):
+			if(house == 0):
 				var NewGrifMale = Data.MaleGryffindor.instance()
 				if(network_id == null): NewGrifMale.name = str(get_tree().get_network_unique_id()); else: NewGrifMale.name = str(network_id)
 				NewGrifMale.set_network_master(network_id)
@@ -15,7 +15,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewGrifMale.updatenamelabel()
 				#Teleport.Move_To_Scene(loc, NewGrifMale, locSpawnPos)
 				Teleport.TeleportPos(NewGrifMale, locSpawnPos)
-			if(house == 2):
+			if(house == 1):
 				var NewHuffMale = Data.MaleHufflepuff.instance()
 				if(network_id == null): NewHuffMale.name = str(get_tree().get_network_unique_id()); else: NewHuffMale.name = str(network_id)
 				NewHuffMale.set_network_master(network_id)
@@ -23,7 +23,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewHuffMale.PlayerName = charname
 				NewHuffMale.updatenamelabel()
 				Teleport.TeleportPos(NewHuffMale, locSpawnPos)
-			if(house == 3):
+			if(house == 2):
 				var NewClawfMale = Data.MaleRavenclaw.instance()
 				if(network_id == null): NewClawfMale.name = str(get_tree().get_network_unique_id()); else: NewClawfMale.name = str(network_id)
 				NewClawfMale.set_network_master(network_id)
@@ -31,7 +31,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewClawfMale.PlayerName = charname
 				NewClawfMale.updatenamelabel()
 				Teleport.TeleportPos(NewClawfMale, locSpawnPos)
-			if(house == 4):
+			if(house == 3):
 				var NewSlythMale = Data.MaleSlytherin.instance()
 				if(network_id == null): NewSlythMale.name = str(get_tree().get_network_unique_id()); else: NewSlythMale.name = str(network_id)
 				NewSlythMale.set_network_master(network_id)
@@ -39,8 +39,8 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewSlythMale.PlayerName = charname
 				NewSlythMale.updatenamelabel()
 				Teleport.TeleportPos(NewSlythMale, locSpawnPos)
-		if(gender == 2):
-			if(house == 1):
+		if(gender == 1):
+			if(house == 0):
 				var NewGrifFemale = Data.FemaleGryffindor.instance()
 				if(network_id == null): NewGrifFemale.name = str(get_tree().get_network_unique_id()); else: NewGrifFemale.name = str(network_id)
 				NewGrifFemale.set_network_master(network_id)
@@ -48,7 +48,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewGrifFemale.PlayerName = charname
 				NewGrifFemale.updatenamelabel()
 				Teleport.TeleportPos(NewGrifFemale, locSpawnPos)
-			if(house == 2):
+			if(house == 1):
 				var NewHuffFemale = Data.FemaleHufflepuff.instance()
 				if(network_id == null): NewHuffFemale.name = str(get_tree().get_network_unique_id()); else: NewHuffFemale.name = str(network_id)
 				NewHuffFemale.set_network_master(network_id)
@@ -56,7 +56,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewHuffFemale.PlayerName = charname
 				NewHuffFemale.updatenamelabel()
 				Teleport.TeleportPos(NewHuffFemale, locSpawnPos)
-			if(house == 3):
+			if(house == 2):
 				var NewClawFemale = Data.FemaleRavenclaw.instance()
 				if(network_id == null): NewClawFemale.name = str(get_tree().get_network_unique_id()); else: NewClawFemale.name = str(network_id)
 				NewClawFemale.set_network_master(network_id)
@@ -64,7 +64,7 @@ remotesync func CreateThePlayer(charname,gender,house, loc, locSpawnPos, network
 				NewClawFemale.PlayerName = charname
 				NewClawFemale.updatenamelabel()
 				Teleport.TeleportPos(NewClawFemale, locSpawnPos)
-			if(house == 4):
+			if(house == 3):
 				var NewSlythFemale = Data.FemaleSlytherin.instance()
 				NewSlythFemale.set_network_master(network_id)
 				if(network_id == null): NewSlythFemale.name = str(get_tree().get_network_unique_id()); else: NewSlythFemale.name = str(network_id)
