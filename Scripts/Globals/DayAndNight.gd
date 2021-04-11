0065f5b4ce4f2c9d556952e26780a4d7b7378f5f
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-11 19:19:13 +0200

    Adding a new and improved lighting/Day and Night system

--- Scripts/Globals/DayAndNight.gd
+++ Scripts/Globals/DayAndNight.gd
@@ -1,4 +1,13 @@
-extends Node
+extends CanvasModulate
+
+export(float) var ShortDuration = 6
+export(float) var LongDuration = 12
+export(Color) var DayColor
+export(Color) var NightColor
+export(Color) var DawnColor
+export(Color) var MidnightColor
+export(Color) var BloodMoon
+
 
 enum TimeOfDay  {
 	Day,
@@ -8,13 +17,16 @@ enum TimeOfDay  {
 	Midnight	
 }
 
-var TimeLong = 120
-var TimeShort = 60
+#How long do days/nights take?
+#(Max) : Values are too low for now for testing. Night and day times should be longer on release
+var TimeLong  : float = 120
+var TimeShort : float = 60
 
 var currentTime = TimeOfDay.Day
 
 var timer
 
+var allowOldSystem : bool = false
 
 func _ready():
 	timer = Timer.new()
@@ -23,30 +35,57 @@ func _ready():
 	timer.wait_time = TimeShort
 	timer.one_shot = false
 	timer.start()
-	print("[WORLD] : Day And Night is ready!")
-	print("[WORLD] : Timer wait_time = " + str(timer.wait_time))
+	print_debug("[WORLD] : Day And Night is ready!")
+	print_debug("[WORLD] : Timer wait_time = " + str(timer.wait_time))
 	
-func UpdateAllNodes(Time):
-	var DNN_Nodes = get_tree().get_nodes_in_group("DNN")
-	for node in DNN_Nodes:
-		node.ChangeColor(Time)
+func OLD_UpdateAllNodes(Time):
+	if(allowOldSystem):
+		var DNN_Nodes = get_tree().get_nodes_in_group("DNN")
+		for node in DNN_Nodes:
+			node.ChangeColor(Time)
+		print_debug("[DayAndNight/warning] - Deprycated system that will be removed in the future has been called, please remove any traces of the old system.")
+	else:
+		print_debug("[DayAndNight/warning] - Deprycated system called while functionality is disabled.")
+
+func ChangeColor(color):
+	match color:
+		"Day":
+			$Tween.interpolate_property(self, "color", self.color, DayColor, ShortDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Night":
+			$Tween.interpolate_property(self, "color", self.color, NightColor, LongDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Dawn":
+			$Tween.interpolate_property(self, "color", self.color, DawnColor, LongDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Midnight":
+			$Tween.interpolate_property(self, "color", self.color, MidnightColor, ShortDuration,Tween.TRANS_CUBIC,Tween.EASE_IN_OUT)
+			$Tween.start()
+		"Bloodmoon":
+			$Tween.interpolate_property(self, "color", self.color, MidnightColor, ShortDuration,Tween.TRANS_BOUNCE,Tween.EASE_IN_OUT)
+			$Tween.start()
 
 func ChangeTime(Time):
 	match Time:
 		TimeOfDay.Day:
-			UpdateAllNodes("Day")
+			OLD_UpdateAllNodes("Day")
+			ChangeColor("Day")
 			currentTime = TimeOfDay.Day
 		TimeOfDay.Dusk:
-			UpdateAllNodes("Dawn")
+			OLD_UpdateAllNodes("Dawn")
+			ChangeColor("Dawn")
 			currentTime = TimeOfDay.Dusk
 		TimeOfDay.Night:
-			UpdateAllNodes("Night")
+			OLD_UpdateAllNodes("Night")
+			ChangeColor("Night")
 			currentTime = TimeOfDay.Night
 		TimeOfDay.Midnight:
-			UpdateAllNodes("Midnight")
+			OLD_UpdateAllNodes("Midnight")
+			ChangeColor("Midnight")
 			currentTime = TimeOfDay.Midnight
 		TimeOfDay.Dawn:
-			UpdateAllNodes("Dawn")
+			OLD_UpdateAllNodes("Dawn")
+			ChangeColor("Dawn")
 			currentTime = TimeOfDay.Dawn
 
 func _on_timer_timeout():
