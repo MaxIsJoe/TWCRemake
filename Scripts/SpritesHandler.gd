869fd542c22693a03aedaf9757ab5645a3774386
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-16 11:17:24 +0200

    Fixes unwanted error spam when calling PlayAllDirAnims()

--- Scripts/SpritesHandler.gd
+++ Scripts/SpritesHandler.gd
@@ -10,28 +10,29 @@ func PlayAnim(anim: String, target: AnimatedSprite):
 	
 func PlayDirectionalAnimAll(Dir: int):
 	for anim in get_children():
-		match Dir:
-			0:
-				anim.play("idledown")
-				currentDir = 0
-			1:
-				anim.play("idleleft")
-				currentDir = 1
-			2:
-				anim.play("idleright")
-				currentDir = 2
-			3:
-				anim.play("idleup")
-				currentDir = 3
-			10:
-				anim.play("walkdown")
-				currentDir = 10
-			11:
-				anim.play("walkleft")
-				currentDir = 11
-			12:
-				anim.play("walkright")
-				currentDir = 12
-			13:
-				anim.play("walkup")
-				currentDir = 13
+		if(anim.frames != null):
+			match Dir:
+				0:
+					anim.play("idledown")
+					currentDir = 0
+				1:
+					anim.play("idleleft")
+					currentDir = 1
+				2:
+					anim.play("idleright")
+					currentDir = 2
+				3:
+					anim.play("idleup")
+					currentDir = 3
+				10:
+					anim.play("walkdown")
+					currentDir = 10
+				11:
+					anim.play("walkleft")
+					currentDir = 11
+				12:
+					anim.play("walkright")
+					currentDir = 12
+				13:
+					anim.play("walkup")
+					currentDir = 13
