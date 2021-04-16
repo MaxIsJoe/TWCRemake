713fd859507107c86f56967dd4bdbccd6b7c2d26
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-16 11:11:40 +0200

    Added a general sprite handler

---
+++ Scripts/SpritesHandler.gd
@@ -0,0 +1,37 @@
+extends Node2D
+
+var currentDir : int
+
+func LoadAnimatedSprites(sprites: String, target: AnimatedSprite):
+	target.frames = load(sprites)
+	
+func PlayAnim(anim: String, target: AnimatedSprite):
+	target.play(anim)
+	
+func PlayDirectionalAnimAll(Dir: int):
+	for anim in get_children():
+		match Dir:
+			0:
+				anim.play("idledown")
+				currentDir = 0
+			1:
+				anim.play("idleleft")
+				currentDir = 1
+			2:
+				anim.play("idleright")
+				currentDir = 2
+			3:
+				anim.play("idleup")
+				currentDir = 3
+			10:
+				anim.play("walkdown")
+				currentDir = 10
+			11:
+				anim.play("walkleft")
+				currentDir = 11
+			12:
+				anim.play("walkright")
+				currentDir = 12
+			13:
+				anim.play("walkup")
+				currentDir = 13
