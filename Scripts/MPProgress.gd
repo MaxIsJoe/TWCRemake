f075e5515ebd86a56755bf4b65b6fb3265d0f7f7
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-04-29 14:53:46 +0200

    Fixes a bug where `$Tween` is null for some reason

--- Scripts/MPProgress.gd
+++ Scripts/MPProgress.gd
@@ -1,10 +1,8 @@
 extends TextureProgress
 
-onready var tween = $Tween
-
 func _on_Player_mpupdate(mana, mana_max):
 	max_value = mana_max
-	tween.interpolate_property(self, "value",
+	$Tween.interpolate_property(self, "value",
 		value, mana, 1,
 		Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
-	tween.start()
+	$Tween.start()
