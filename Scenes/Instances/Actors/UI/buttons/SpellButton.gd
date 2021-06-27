765ca1e2a1f0f279fe308710132750dd2777aaa1
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-06-27 11:04:14 +0200

    Reworked UI

--- Scenes/Instances/Actors/UI/buttons/SpellButton.gd
+++ Scenes/Instances/Actors/UI/buttons/SpellButton.gd
@@ -3,8 +3,8 @@ extends Button
 var SpellID
 
 func UpdateButton(Name, Icon, ID):
-	$RichTextLabel.bbcode_text = str("[center]" + Name)
-	$SpellIcon.texture = load(Icon)
+	text = Name
+	icon = load(Icon)
 	SpellID = ID
 
 func _on_SpellButton_button_down():
