7c20ec9152250aeb3f62dd75e34316fd5af06733
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-08-06 13:06:20 +0200

    Grayscaling vision when near death

--- Scenes/Instances/Actors/player/Health.gd
+++ Scenes/Instances/Actors/player/Health.gd
@@ -2,7 +2,7 @@ extends "res://Scenes/Instances/EntitesBase/Characters/Health.gd"
 
 export(NodePath) var HealthBar_PATH
 onready var HealthBar = get_node(HealthBar_PATH)
-	
+export(bool) var CanGrayscaleVision = true
 	
 func _ready():
 	HealthBar._on_Player_hpupdate(HP, HP_MAX)
@@ -14,3 +14,4 @@ remotesync func TakeDamage(damage: int, damageBy: String = "unkown"):
 remotesync func BecomeAlive():
 	.BecomeAlive()
 	HealthBar._on_Player_hpupdate(HP, HP_MAX)
+
--- Scripts/HPProgress.gd
+++ Scripts/HPProgress.gd
@@ -1,9 +1,31 @@
 extends TextureProgress
 
+export(NodePath) var HealthOverlayPath
+export(NodePath) var HealthPath
+
+var Health
+var HealthOverlay
+var material_onOverlay
+
+func _ready():
+	HealthOverlay = get_node(HealthOverlayPath)
+	Health = get_node(HealthPath)
+	material_onOverlay = HealthOverlay.get_material()
+
 func _on_Player_hpupdate(health, max_health):
 	max_value = max_health
 	$Tween.interpolate_property(self, "value",
 		value, health, 1,
 		Tween.EASE_OUT , Tween.EASE_IN_OUT)
 	$Tween.start()
+	GrayScaleVision()
 
+
+##Notes about grayscale##
+#Tweening this causes the screen to flicker too much when receiving too much damage
+#avoid tweening this for now until we find a way to not cause epilepsy
+func GrayScaleVision():
+	if(Health != null):
+		if(Health.CanGrayscaleVision == true):
+			var grayscale_value : float = float((Health.HP_MAX - Health.HP)) / 100
+			material_onOverlay.set_shader_param("scale",grayscale_value)
