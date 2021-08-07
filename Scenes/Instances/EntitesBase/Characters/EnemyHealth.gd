59ce70d3dc4d09e8bd4262a443cd912faf9196cd
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2021-08-07 03:06:41 +0200

    node path assignment correction

--- Scenes/Instances/EntitesBase/Characters/EnemyHealth.gd
+++ Scenes/Instances/EntitesBase/Characters/EnemyHealth.gd
@@ -1,16 +1,11 @@
 extends Entity_Health
 
-export(NodePath) var enemyBasePath
 
-var EnemeyBase : Enemy_Entity
-
-func _ready():
-	EnemeyBase = get_node(enemyBasePath)
 
 remotesync func TakeDamage(damage: int, damagedBy):
 	.TakeDamage(damage, damagedBy)
-	if(EnemeyBase.current_state == EnemeyBase.AI_states.IDLE or 
-	EnemeyBase.current_state == EnemeyBase.AI_states.RETREAT or 
-	EnemeyBase.current_state == EnemeyBase.AI_states.WANDER):
+	if(parent.current_state == parent.AI_states.IDLE or 
+	parent.current_state == parent.AI_states.RETREAT or 
+	parent.current_state == parent.AI_states.WANDER):
 		if(damagedBy is PlayerEntity):
-			EnemeyBase.SetAttackTarget(damagedBy)
+			parent.SetAttackTarget(damagedBy)
